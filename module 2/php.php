bf8893c429f816459d3f240b7f7eb187d347c893
erjon0 <erjonbenquku@gmail.com>
2025-01-23 16:18:45 +0100

    sahid

---
+++ module 2/php.php
@@ -0,0 +1,16 @@
+<?php
+
+
+ $COOK = 'cookie';
+ $vanilla = 'vanilla';
+ $puding = 'puding';
+ $chokolata = 'chokolata';
+ $bullion = 15;
+
+echo = $COOK;
+echo = $vanilla;
+echo = $puding;
+echo = $chokolata;
+
+
+?>
