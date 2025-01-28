b11877d2487d328f7fe49156e950986257d3f7c3
erjon0 <erjonbenquku@gmail.com>
2025-01-28 16:18:26 +0100

    asdkad

---
+++ module 3/php.php
@@ -0,0 +1,24 @@
+<?php
+$grade = 79
+
+if($grade = 70);
+    echo("you got a 5");
+
+elseif($grade >= 60);
+    echo("you got a 4");
+
+elseif($grade >= 50);
+    echo("you got a 3");
+
+elseif($grade >= 20);
+    echo("you got a 2");
+
+
+
+    $day = "monday"
+    
+    switch($day)
+    case()
+?>
+
+
