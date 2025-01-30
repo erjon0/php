a83f51e06df67d5a1262094ca7ed13b0d3200bbe
erjon0 <erjonbenquku@gmail.com>
2025-01-30 16:18:26 +0100

    z\x\z

---
+++ module 3/pj.php
@@ -0,0 +1,17 @@
+<?php
+$student = ["andi" ,"andi" ,"andi" ,"andi" ,"andi" ,"andi" ,]
+
+
+
+for($x = 50 , $ =< 100 ; $x = $x + 2){
+    echo($x);
+}
+
+foreach($student as $student){
+    echo($student."<br>")
+}
+
+
+
+
+?>
