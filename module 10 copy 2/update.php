3815067aae08df6ec930eaa81d7d850a4eed18b8
erjon0 <erjonbenquku@gmail.com>
2025-03-20 15:12:31 +0100

    dsadsadas

--- module 10 copy 2/update.php
+++ module 10 copy 2/update.php
@@ -1,5 +1,40 @@
 <?php
 include_once("config.php");
 
+if(isset($_POST['update']))
+{	
+    $ids = $_POST['id'];
+    
+    $name=$_POST['name'];
+    $age=$_POST['age'];
+    $email=$_POST['email'];	
+    $surname=$_POST['surname'];
+    
+    // checking empty fields
+    if(empty($name) || empty($age) || empty($email) || empty($surname)) {	
+            
+        if(empty($name)) {
+            echo "<font color='red'>Name field is empty.</font><br/>";
+        }
+        
+        if(empty($age)) {
+            echo "<font color='red'>Age field is empty.</font><br/>";
+        }
+        
+        if(empty($email)) {
+            echo "<font color='red'>Email field is empty.</font><br/>";
+        }		
+        if(empty($surname)) {
+            echo "<font color='red'>Surname field is empty.</font><br/>";
+        }
+
+    } else {	
+        //updating the table
+        $result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
+        
+        //redirectig to the display page. In our case, it is index.php
+        header("Location: index.php");
+    }
+}
 
 ?>
